5cdf2676b5f90ec3d03d0ab00a2727b6584267d4
Nikita Ostapenko <nikitul2005@gmail.com>
2025-02-20 22:29:23 -0500

    :art: make dropdown funcitonality, add cover for transition

--- script.js
+++ script.js
@@ -5,10 +5,35 @@ async function main() {
 	}
 	let coordinates;
 	let tempname = "fahrenheit";
-	await navigator.geolocation.getCurrentPosition(async (geolocation) => {
+	let dropdownOpen = false;
+	const dropdownMenu = document.getElementById("dropdown");
+	dropdownMenu.addEventListener("click", () => {
+		if (dropdownOpen) {
+			dropdownMenu.classList.remove("open");
+			dropdownOpen = false;
+		} else {
+			dropdownMenu.classList.add("open");
+			dropdownOpen = true;
+		}
+	});
+	document.getElementById("show-data").addEventListener("click", () => navigator.geolocation.getCurrentPosition(async (geolocation) => {
+		const cover = document.getElementById("cover");
+		cover.classList.add("blockout");
+		cover.classList.add("z-index-fix");
+		const timer = new Promise(resolve => setTimeout(resolve, 1000));
 		coordinates = geolocation.coords;
-		const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${coordinates.latitude}&longitude=${coordinates.longitude}&current=temperature_2m,relative_humidity_2m,precipitation,rain,showers,snowfall,weather_code,wind_speed_10m,wind_direction_10m,wind_gusts_10m,is_day&temperature_unit=${tempname}`);
-		const data = await response.json();
+		let dataVar;
+		const request = (async () => {
+			const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${coordinates.latitude}&longitude=${coordinates.longitude}&current=temperature_2m,relative_humidity_2m,precipitation,rain,showers,snowfall,weather_code,wind_speed_10m,wind_direction_10m,wind_gusts_10m,is_day&temperature_unit=${tempname}`);
+			return await response.json();
+		})()
+		request.then(data => dataVar = data);
+		await Promise.all([request, timer]);
+		setTimeout(() => {
+			cover.classList.remove("z-index-fix");
+		}, 1000);
+		const data = dataVar;
+		cover.classList.remove("blockout");
 		console.log(data);
 		const current = data.current;
 		console.log(current);
@@ -21,8 +46,10 @@ async function main() {
 		document.getElementById("latitude").textContent = `${coordinates.latitude}`;
 		document.body.classList.add("show-data");
 		if (current.is_day === 0) {
-			document.body.classList.add("is-daylight");
+			document.body.classList.add("is-night");
+		} else {
+			document.body.classList.add("is-daylight")
 		}
-	});
+	}), {once: true});
 }
 main();
